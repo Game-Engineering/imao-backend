a391f32b8cb8eefd515a387dfd386ffa91cda269
Janneck Lange <langejanneck@gmail.com>
2018-06-05 16:04:33 +0200

    ✨ pressekonferenz erstellt

--- src/main/java/de/hsmannheim/ss18/gae/imao/model/PressekonferenzThema.java
+++ src/main/java/de/hsmannheim/ss18/gae/imao/model/PressekonferenzThema.java
@@ -1,5 +1,76 @@
 package de.hsmannheim.ss18.gae.imao.model;
 
-public class PressekonferenzThema {
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
+public class PressekonferenzThema {
+	
+	private int id;
+	private String thema;
+	private int maxAnsehen;
+	private int schwierigkeit;
+	
+	private Frage[] fragen;
+	private boolean verfuegbar = false;
+	
+	
+	/**
+	 * 
+	 * @param id
+	 * @param thema
+	 * @param maxAnsehen
+	 * @param schweirigkeit
+	 * @param fragen
+	 */
+	public PressekonferenzThema(int id, String thema, int maxAnsehen, int schweirigkeit,Frage[] fragen) {
+		this.id = id;
+		this.thema = thema;
+		this.maxAnsehen = maxAnsehen;
+		this.schwierigkeit = schweirigkeit;
+		this.fragen=fragen;
+	}
+	
+	
+	public int getId() {
+		return id;
+	}
+
+	public String getThema() {
+		return thema;
+	}
+
+	public int getMaxAnsehen() {
+		return maxAnsehen;
+	}
+
+	public int getSchwierigkeit() {
+		return schwierigkeit;
+	}
+
+	public Frage[] getFragen() {
+		return fragen;
+	}
+
+	public boolean isVerfuegbar() {
+		return verfuegbar;
+	}
+	
+	public void setVerfuegbar(boolean verfuegbar) {
+		this.verfuegbar=verfuegbar;
+	}
+	
+	@Override
+	public String toString() {
+		ObjectMapper mapper = new ObjectMapper();
+
+		ObjectNode objectNode = mapper.createObjectNode();
+		objectNode.put("ID", getId());
+		objectNode.put("Thema", getThema());
+		objectNode.put("maxAnsehen", getMaxAnsehen());
+		objectNode.put("schwierigkeit", getSchwierigkeit());
+		objectNode.put("Verfuegbar", isVerfuegbar());
+
+		return objectNode.toString();
+	}
+	
 }
