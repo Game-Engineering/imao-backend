2f5c883e4f9c3f7fbd6553977ba9ec94b86a555a
Friederike Herbold <f.herbold@googlemail.com>
2018-06-14 14:22:44 +0200

    Anpassung für Monnem Bike wieder entfernt

--- src/main/java/de/hsmannheim/ss18/gae/imao/model/wirtschaft/Aufgabe.java
+++ src/main/java/de/hsmannheim/ss18/gae/imao/model/wirtschaft/Aufgabe.java
@@ -29,103 +29,74 @@ public class Aufgabe {
 	 * @param runde
 	 */
 	private void neueAufgabe(int runde) {
-		if (runde <= 3) {
-			switch (runde) {
-			case 1:
-				if (this.interview.kannAufgabeSein()) {
-					aufgabe = EAufgaben.INTERVIEW;
-					rufschaden = 10;
-				} else {
-					this.neueAufgabe(runde);
-				}
-				aufgabeBetreff = "I";
-				break;
-			case 2:
-				aufgabe = EAufgaben.ARZT_ABMAHNEN;
+		switch (new Random().nextInt(7)) {
+		case 0:
+			if (this.interview.kannAufgabeSein()) {
+				aufgabe = EAufgaben.INTERVIEW;
 				rufschaden = 10;
-				aufgabeBetreff = "A";
-				break;
-			case 3:
-				aufgabe = EAufgaben.PRESSEKONFERENZ_GUTEARBEIT;
-				rufschaden = 0;
-				for (int i = 0; i < pressekonferenz.getPressekonferenzThemen().length; i++) {
-					if (this.pressekonferenz.getPressekonferenzThemen()[i].getId() == 2) {
-						this.pressekonferenz.getPressekonferenzThemen()[i].setVerfuegbar(true);
-					}
-				}
-				aufgabeBetreff = "PK";
+			} else {
+				this.neueAufgabe(runde);
 			}
-
-		} else {
-			switch (new Random().nextInt(7)) {
-			case 0:
-				if (this.interview.kannAufgabeSein()) {
-					aufgabe = EAufgaben.INTERVIEW;
-					rufschaden = 10;
-				} else {
-					this.neueAufgabe(runde);
+			aufgabeBetreff = "I";
+			break;
+		case 1:
+			aufgabe = EAufgaben.PRESSEKONFERENZ_DUERRE;
+			rufschaden = 10;
+			for (int i = 0; i < pressekonferenz.getPressekonferenzThemen().length; i++) {
+				if (this.pressekonferenz.getPressekonferenzThemen()[i].getId() == 3) {
+					this.pressekonferenz.getPressekonferenzThemen()[i].setVerfuegbar(true);
 				}
-				aufgabeBetreff = "I";
-				break;
-			case 1:
-				aufgabe = EAufgaben.PRESSEKONFERENZ_DUERRE;
-				rufschaden = 10;
-				for (int i = 0; i < pressekonferenz.getPressekonferenzThemen().length; i++) {
-					if (this.pressekonferenz.getPressekonferenzThemen()[i].getId() == 3) {
-						this.pressekonferenz.getPressekonferenzThemen()[i].setVerfuegbar(true);
-					}
-				}
-				aufgabeBetreff = "PK";
-				break;
-			case 5:
-				aufgabe = EAufgaben.PRESSEKONFERENZ_GUTEARBEIT;
-				rufschaden = 0;
-				for (int i = 0; i < pressekonferenz.getPressekonferenzThemen().length; i++) {
-					if (this.pressekonferenz.getPressekonferenzThemen()[i].getId() == 2) {
-						this.pressekonferenz.getPressekonferenzThemen()[i].setVerfuegbar(true);
-					}
-				}
-				aufgabeBetreff = "PK";
-				break;
-			case 6:
-				aufgabe = EAufgaben.PRESSEKONFERENZ_VIELETOTE;
-				rufschaden = 20;
-				for (int i = 0; i < pressekonferenz.getPressekonferenzThemen().length; i++) {
-					if (this.pressekonferenz.getPressekonferenzThemen()[i].getId() == 1) {
-						this.pressekonferenz.getPressekonferenzThemen()[i].setVerfuegbar(true);
-					}
-				}
-				aufgabeBetreff = "PK";
-				break;
-			// case 2:
-			// aufgabe = EAufgaben.GREAET_KAUFEN;
-			// rufschaden = 10;
-			// break;
-			case 3:
-				if (this.sponsoren.kannAufgabeSein()) {
-					aufgabe = EAufgaben.SPONSOR_ANWERBEN;
-					rufschaden = 5;
-				} else {
-					this.neueAufgabe(runde);
-				}
-				aufgabeBetreff = "SP";
-				break;
-			case 4:
-				aufgabe = EAufgaben.ARZT_ABMAHNEN;
-				rufschaden = 10;
-				aufgabeBetreff = "A";
-				break;
-			default:
-				aufgabe = EAufgaben.KEINE;
-				rufschaden = 0;
-				aufgabeBetreff = "OK";
 			}
+			aufgabeBetreff = "PK";
+			break;
+		case 5:
+			aufgabe = EAufgaben.PRESSEKONFERENZ_GUTEARBEIT;
+			rufschaden = 0;
+			for (int i = 0; i < pressekonferenz.getPressekonferenzThemen().length; i++) {
+				if (this.pressekonferenz.getPressekonferenzThemen()[i].getId() == 2) {
+					this.pressekonferenz.getPressekonferenzThemen()[i].setVerfuegbar(true);
+				}
+			}
+			aufgabeBetreff = "PK";
+			break;
+		case 6:
+			aufgabe = EAufgaben.PRESSEKONFERENZ_VIELETOTE;
+			rufschaden = 20;
+			for (int i = 0; i < pressekonferenz.getPressekonferenzThemen().length; i++) {
+				if (this.pressekonferenz.getPressekonferenzThemen()[i].getId() == 1) {
+					this.pressekonferenz.getPressekonferenzThemen()[i].setVerfuegbar(true);
+				}
+			}
+			aufgabeBetreff = "PK";
+			break;
+		// case 2:
+		// aufgabe = EAufgaben.GREAET_KAUFEN;
+		// rufschaden = 10;
+		// break;
+		case 3:
+			if (this.sponsoren.kannAufgabeSein()) {
+				aufgabe = EAufgaben.SPONSOR_ANWERBEN;
+				rufschaden = 5;
+			} else {
+				this.neueAufgabe(runde);
+			}
+			aufgabeBetreff = "SP";
+			break;
+		case 4:
+			aufgabe = EAufgaben.ARZT_ABMAHNEN;
+			rufschaden = 10;
+			aufgabeBetreff = "A";
+			break;
+		default:
+			aufgabe = EAufgaben.KEINE;
+			rufschaden = 0;
+			aufgabeBetreff = "OK";
 		}
 		sendeAufgabenMail(runde);
 	}
 
 	private void sendeAufgabenMail(int runde) {
-		aufgabenMail = new Mail("CHEF", "R" + runde+" "+aufgabeBetreff, aufgabe.getAufgabeText());
+		aufgabenMail = new Mail("CHEF", "R" + runde + " " + aufgabeBetreff, aufgabe.getAufgabeText());
 	}
 
 	public void erledigt() {
